03580d7854ae2bac69062f069459797117aa3463
Tanjid Hossen <64518451+tanjid-showkot@users.noreply.github.com>
2023-02-17 01:57:12 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,42 @@
+import requests
+import telebot
+import os
+token = os.environ.get('API_KEY')
+bot = telebot.TeleBot(token)
+
+
+
+
+url = "https://pinksale-trending.s3.amazonaws.com/trending.json"
+response = requests.get(url)
+
+
+@bot.message_handler(['start'])
+def start(message):
+    bot.reply_to(message, "welcome to this bot. type /list ")
+
+
+@bot.message_handler(['list'])
+def list_tokens(message):
+    prio = sto(response)
+    token_list = ""
+    for x, prios in enumerate(prio):
+        token_list += f"{x+1}: {prios}\n"
+    bot.reply_to(message, token_list)
+
+
+def sto(response):
+    store = []
+
+    if response.status_code == 200:
+        data = response.json()
+        for x in range(len(data["data"])):
+            store.append(data["data"][x]["token"])
+        return store
+      
+
+    else:
+        return f"Failed to fetch data from {url}. Status code: {response.status_code}"
+
+
+bot.polling()
