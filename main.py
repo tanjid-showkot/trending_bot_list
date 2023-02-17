5a42891969d82e4f6ef186e284b6f2404993a7ef
Tanjid Hossen <64518451+tanjid-showkot@users.noreply.github.com>
2023-02-17 20:44:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import telebot
 import os
+import time
 token = os.environ.get('API_KEY')
 bot = telebot.TeleBot(token)
 
@@ -8,7 +9,7 @@ bot = telebot.TeleBot(token)
 
 
 url = "https://pinksale-trending.s3.amazonaws.com/trending.json"
-response = requests.get(url)
+
 
 
 @bot.message_handler(['start'])
@@ -18,25 +19,27 @@ def start(message):
 
 @bot.message_handler(['list'])
 def list_tokens(message):
-    prio = sto(response)
+    prio = sto(url)
     token_list = ""
     for x, prios in enumerate(prio):
         token_list += f"{x+1}: {prios}\n"
     bot.reply_to(message, f"{token_list}\n Devoloped by @dev_andrei")
 
 
-def sto(response):
+def sto(url):
+    
     store = []
-
-    if response.status_code == 200:
-        data = response.json()
-        for x in range(len(data["data"])):
-            store.append(data["data"][x]["token"])
-        return store
+    while True:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            for x in range(len(data["data"])):
+                store.append(data["data"][x]["token"])
+            return store
+        time.sleep(60)
       
 
-    else:
-        return f"Failed to fetch data from {url}. Status code: {response.status_code}"
+  
 
 
 bot.polling()
